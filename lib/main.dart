080a2a71f13adb480c7094afbd7e24b39ca7851b
Nipul Rathod <nipulrathod5112@gmail.com>
2024-09-29 19:58:25 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:dictionary_app/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Dictionary',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      home: HomePage(),
+    );
+  }
+}
